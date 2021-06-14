31b4c8824b0813edb77cd897c79e82d1b43be9e2
Robert <robertasbaronas@hotmail.co>
2021-06-14 16:20:30 +0300

    Initial Commit

---
+++ MVVMCombineSwiftUI/MVVMCombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMCombineSwiftUIApp.swift
+//  MVVMCombineSwiftUI
+//
+//  Created by Robertas Baronas on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MVVMCombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
